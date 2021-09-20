9a0dff5bb8c9f4091b378267f2195c3b447c2a8e
madan <madan@local.com>
2021-09-20 23:09:51 +0530

    added code to validate the Last name

---
+++ src/com/userregistrationtest/userRegistrationTest.java
@@ -0,0 +1,24 @@
+package com.userregistrationtest;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.userregistration.UserRegistration;
+
+public class userRegistrationTest {
+	
+	@Test
+	public void testValidateFirstNameShouldReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean output = userRegistration.validateFirstName("Madan");
+		Assert.assertTrue(output);
+	}
+	
+	@Test
+	public void testValidateLastNameShouldReturnTrue() {
+		UserRegistration userRegistration = new UserRegistration();
+		boolean output = userRegistration.validateLastName("Hello");
+		Assert.assertTrue(output);
+	}
+	
+}
